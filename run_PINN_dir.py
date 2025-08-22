72ac4e7b9dad68fa2f5caad6f5829683d59e541c
Anon <kim.bente@icloud.com>
2025-08-23 07:36:24 +1000

    Update run_PINN_dir.py path

--- run_PINN_dir.py
+++ run_PINN_dir.py
@@ -66,10 +66,9 @@ print("y_test shape:", y_test.shape)
 print()
 
 ### UNIT VELOCITIES OVER DOMAIN ###
-# test_velocity_unit_norm = torch.load("data/directional_guidance_test/test_velocity_unit_norm.pt", 
-#                                     weights_only = False).to(device)
-velocity_unit_norm = torch.load("data/directional_guidance_test/velocity_unit_norm.pt", 
+velocity_unit_norm = torch.load("data/directional_guidance/velocity_unit_norm.pt", 
                                      weights_only = False).to(device)
+
 print("velocity_unit_norm shape:", velocity_unit_norm.shape)
 # Extract number of velocities as upper bound
 N_dg = velocity_unit_norm.shape[0]
